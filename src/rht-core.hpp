9e6784c92cb11039d7512058512d67446b18e0c7
Veselin Karaganev <vesko.karaganev@gmail.com>
2019-08-03 14:46:57 +0300

    Moved source files to /src

---
+++ src/rht-core.hpp
@@ -0,0 +1,56 @@
+/**
+ * Created by veselink1.
+ * Released under the MIT license.
+ */
+
+#ifndef REFL_HT_CORE_HPP
+#define REFL_HT_CORE_HPP
+#include <clang-c/Index.h>
+#include "rht-util.hpp"
+
+namespace rht::core
+{
+enum MemberType
+{
+    Field,
+    Function
+};
+
+struct MemberInfo
+{
+    MemberType Type;
+    std::string Name;
+    std::string Attributes;
+
+    MemberInfo(MemberType type, const std::string& name, const std::string& attributes)
+        : Type(type), Name(name), Attributes(attributes)
+    {
+    }
+};
+
+struct TypeInfo
+{
+    std::string Name;
+    std::string Attributes;
+    std::vector<MemberInfo> Members;
+
+    TypeInfo(const std::string& name, const std::string& attributes)
+        : Name(name), Attributes(attributes), Members{}
+    {
+    }
+};
+
+struct ModuleInfo
+{
+    std::string Path;
+    std::vector<core::TypeInfo> Types;
+
+    ModuleInfo(const std::string& path, const std::vector<core::TypeInfo>& types)
+        : Path(path), Types(types)
+    {
+    }
+};
+
+} // namespace rht::core
+
+#endif // REFL_HT_CORE_HPP
